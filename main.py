1cb1fc9feecf85bd503b33ab037fa8b48734f968
Alezandah <alexokereke47@gmail.com>
2021-09-12 21:44:12 +0000

    Testing Digital Clock

---
+++ main.py
@@ -0,0 +1,8 @@
+import sqlite3
+con = sqlite3.connect('example.db')
+from mysql.connector import Error
+
+# mydb = mysql.connector.connect(host="localhost", user="root", passwd="password123")
+
+#mydb.close()
+# print (mydb)
